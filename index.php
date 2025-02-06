ed57c48f8ce6e5d238eedeb4c1179fdb355e1e39
Tewobsta Seyoum Assefa <155534908+Tewobsta-sa@users.noreply.github.com>
2025-02-06 22:19:42 +0300

    completed

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ button:hover {
 </head>
 <body>
     <h2>Login</h2>
-    <form method="POST" action="login.php">
+    <form method="POST" action="index.php">
         <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
         <button type="submit">Login</button>
